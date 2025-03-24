066ee8915d3c4ae66b1035d639e4c5f3851c6fd8
ore552 <osafule@gmail.com>
2025-03-24 19:05:24 +0000

    solving SubPlans error

--- src/Components/SubPlans.jsx
+++ src/Components/SubPlans.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { motion, useAnimation } from 'framer-motion'
-import Aos from 'aos'
+import {SectionWrapper} from '../hoc'
+import {plans} from "../constants"
 import "aos/dist/aos.css"
-import { plans } from '../constants'
-import { SectionWrapper } from '../hoc'
+import AOS from 'aos'
+import {useEffect} from 'react'
+import {motion, useAnimation} from 'framer-motion'
 
-const SubPlans = () => {
 
-    useEffect (() => {
-        Aos.init({duration: 1500});
+
+function SubPlans() {
+
+    useEffect(() => {
+        AOS.init({duration: 1500});
     }, []);
 
     const controls = useAnimation();
-    
+
     const handleHover = () => {
-        controls.start({ x: [0, -5, 5, 0], transition: {duration: 0.7} });
+        controls.start({ x: [0, -5, 5, -5, 0], transition: {duration: 0.7} });
     };
 
   return (
@@ -24,43 +26,43 @@ const SubPlans = () => {
     data-aos="fade-down"
     data-aos-easing="linear"
     data-aos-duration="1500"
-    className='top flex justify-between items-end md-[7vh] mt-[4vh]'
+    className='top flex justify-between items-end mb-[7vh] mt-[4vh]'
     >
         <h2 className='w-[523px] h-[140px] font-bold text-[60px] leading-[70px] tracking-[-1.88px]'>Get the right plan for future product.</h2>
         <div className='buttons pb-[14px]'>
-            <button className='text-center bg-[#5454D4] w-[160px] h-[55px] text-white'>Yearly</button>
-            <motion.button
-            onHoverStart={handleHover}
-            onHoverEnd={() => controls.start({x: 0})}
-            animate={controls}
-            className='text-center w-[160px] h-[55px] bg-[#F7F7FB]'
-            >Monthly</motion.button>
+            <button className='text-center bg-[#5454D4] w-[160px] h-[55px] text-white'>
+                Yearly</button>
+                <motion.button
+                onHoverStart={handleHover}
+                onHoverEnd={() => controls.start({ x: 0 })}
+                animate={controls}
+                className='text-center w-[160px] h-[55px] bg-[#F7F7FB]'
+                >Monthly</motion.button>
         </div>
     </div>
 
     <div className='bottom'>
-        <div className='flex justify-evenly plans flex-warp'>
-            {plans.map((plan) => (
-                <div
+        <div className='flex justify-evenly plans flex-wrap'>
+            {plans.map(() => {
+                <div 
                 data-aos="flip-up"
                 data-aos-duration="2000"
-                key={plan.title}
-                className='w-[330px] h-[441px] bg-[#F7F7FB] p-[1vw]rounded-xl flex flex-col items-center pt-10'
+                key={plans.title}
+                className='w-[330px] h-[441px] bg-[#F7F7FB] p-[1vw] rounded-xl flex flex-col items-center pt-10'
                 >
-                    <p className='text-[#696871] text[18px] leading-[28px] tracking-[-0.56px] pb-6'>{plan.title}</p>
-                    <h3 className='text-black text[50px] leading-[56px] tracking-[-1.03px] pb-6'>{plan.price}</h3> 
-                    <ul className='flex flex-col'>
-                    {plan.specs.map((spec) => (
-                        <li key={spec} className='text-[#696871] text[17px] leading-[50px] tracking-[-0.53px] text-center'>{spec}</li>
-                    ))}
-                    </ul>
-                    <button className='text-[#5454D4] bg-white w-[285px] h-[60px] leading-[28px] tracking-[-0.56px] font-bold rounded-[12px] mt-6'>{plan.button}</button>      
+                    <p className='text-[#696871] text[18px] leading-[28px] tracking-[-0.56px] pb-6'>{plans.title}</p>
+                    <h3 className='text-black text[18px] leading-[56px] tracking-[-1.03px] pb-6'>{plans.price}</h3>
+                    {/* <ul className='flex flex-col'>
+                        {plans.specs.map((spec) => (
+                            <li key={spec} className='text-[#696871] text[17px] leading-[50px] tracking-[-0.53px] text-center'>{spec}</li>
+                        ))}
+                    </ul> */}
                 </div>
-            ))}
+            })}
         </div>
     </div>
     </>
   )
 }
 
-export default (SubPlans, '')
+export default SubPlans
